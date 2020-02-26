4cda1bd21733e443935413839436d4ca666f92fd
Milosz Milewski <MMilosz122@gmail.com>
2020-02-26 01:31:20 +0000

    added java project

---
+++ Java/src/milosz/Timetable.java
@@ -0,0 +1,84 @@
+package milosz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
+
+public class Timetable extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Timetable frame = new Timetable();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Timetable() {
+		setTitle("Your Shifts");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Monday", null, null, null},
+				{"Tuesday", null, null, null},
+				{"Wednesday", null, null, null},
+				{"Thursday", null, null, null},
+				{"Friday", null, null, null},
+				{"Saturday", null, null, null},
+				{"Sunday", null, null, null},
+			},
+			new String[] {
+				"Day", "Working", "Start Time", "End Time"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Object.class, Boolean.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, true, true, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setFillsViewportHeight(true);
+		table.setRowHeight(32);
+	}
+
+}
