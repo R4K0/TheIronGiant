97614e2671accf9881d59ea6c8a80f15acf1f7ef
SeanSkeltonGMIT <37144812+SeanSkeltonGMIT@users.noreply.github.com>
2020-02-28 13:16:00 +0000

    add 1
    add 1

--- Java/src/milosz/Timetable.java
+++ Java/src/milosz/Timetable.java
@@ -36,7 +36,7 @@ public class Timetable extends JFrame {
 	 * Create the frame.
 	 */
 	public Timetable() {
-		setTitle("Your Shifts");
+		setTitle("Your Shifts 1");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
